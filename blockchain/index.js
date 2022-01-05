45c4bab46d00009e75d3e2520dacc1e53b231dfa
HuakFreeTime <komsark_@hotmail.co.th>
2022-01-05 23:19:03 +0700

    feat: add-thuffle-smart-contract

---
+++ blockchain/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const Web3 = require('web3');
+const TruffleContract = require('@truffle/contract');
+
+// const web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
+// const web3 = new Web3(web3Provider);
+
+// const createContractInstance = async artifactName => {
+//  const artifact = JSON.parse(fs.readFileSync(path.join(__dirname, 'build/contracts', `${artifactName}.json`)));
+//   const contract = TruffleContract(artifact);
+//   contract.setProvider(web3Provider);
+//   return contract.deployed();
+// };
+
+
+
+module.exports = {
+ 
+}
