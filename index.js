804fa6f3e5c5ef8e23992820564c6c6c8ea4e4db
HuakFreeTime <komsark_@hotmail.co.th>
2022-01-05 22:45:19 +0700

    feat: add-router

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 8000
-app.listen()
+const express = require("express"),
+  cors = require("cors");
+  userRoutes = require("./routes/user")
+const app = express();
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(cors());
+app.use("/user", userRoutes);
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
