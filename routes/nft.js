d3337173cd6b39ce883902113e4a1c525f664e72
HuakFreeTime <komsark_@hotmail.co.th>
2022-02-08 01:02:35 +0700

    feat: add-game-get-owner-nft

--- routes/nft.js
+++ routes/nft.js
@@ -185,4 +185,24 @@ router.get("/webhook/get-owner-nft/:address", async (req, res) => {
   }
 });
 
+router.get("/game/get-owner-nft/:address", async (req, res) => {
+  try {
+    const address = req.params.address;
+    User.findOne({ address_wallet: address })
+      .populate("listNFT")
+      .exec(async (error, result) => {
+        if (error) {
+          console.log(error);
+        } else {
+          return res.json({
+            data: result.listNFT,
+            status: "success",
+          });
+        }
+      });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 module.exports = router;
